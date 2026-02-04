ceb5426f827c6e453033017c0f993917e15b16b8
timtjoe <ttjoe0@gmail.com>
2026-02-04 11:09:22 +0000

    change repo name and vite base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/atprotosy/",
+  base: "/atpx/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
